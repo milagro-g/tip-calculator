7ad290a0af3c91cffb4fddd17fc2931ebf216e05
milagro-g <milagrolyew@gmail.com>
2019-07-26 10:37:17 -0400

    completed form and trouble shooting for functionality

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+window.addEventListener('load', function(){
+ console.log("Hello World!");
+
+//this is to manipulate de Dom
+function calculate(){
+ let totalBill = document.getElementById("total-bill").value;
+   // console.log(document.getElementById("total-bill").value);
+  let serviceOptions = document.getElementById("service-options").value;
+  // console.log(serviceOptions);
+  let totalParty = document.getElementById("total-party").value;
+  // console.log(totalParty);
+  let totalTip = totalBill * serviceOptions;
+  // console.log(totalTip);
+  let myTip = totalTip / totalParty;
+  let myTipAmount = document.createElement("P");
+  document.getElementById("tip-content-container").appendChild(node);
+}
+
+let submitButton = document.querySelector('#calculate-button');
+// console.log(submitButton);
+
+submitButton.onclick = function() { calculate(); event.preventDefault(); };
+
+});
